72e3f318e483fcbd253e8b965093c81fc0ca35ff
dendersen <dendersen@endersen.dk>
2024-01-30 16:19:53 +0100

    Fix drawing bug and implement gravity for topMap

--- src/main/java/dk/mtdm/Sketch.java
+++ src/main/java/dk/mtdm/Sketch.java
@@ -7,36 +7,37 @@ public class Sketch extends PApplet {
   private int gravityTimer = 0;
   final int indexWidth = 20;
   Color[][] SubMap = new Color[worldWidth][worldHeight];
-  Color[][] topMap;
+  Color[][] topMap = new Color[worldWidth*3][worldHeight*3];
   boolean[][] lockMap;
   boolean makePiece = true;
   SubSquare activePiece;
   PieceGenerator pg = new PieceGenerator();
-
+  
   public void draw(){
-    clearLines();
+    Color[][] map = makePiece ? SubMap : topMap;
+    clearLines(map);
     background(255);
-    for (int x = 0; x < SubMap.length; x++) {
-      for (int y = 0; y < SubMap[x].length; y++) {
-        if(SubMap[x][y] == null){
+    for (int x = 0; x < map.length; x++) {
+      for (int y = 0; y < map[x].length; y++) {
+        if(map[x][y] == null){
           continue;
         }
-        g.fill(SubMap[x][y].getRed(), SubMap[x][y].getGreen(), SubMap[x][y].getBlue());
-        g.stroke(Math.max(SubMap[x][y].getRed()-20, 0), Math.max(SubMap[x][y].getGreen()-20, 0), Math.max(SubMap[x][y].getBlue()-20, 0));
+        g.fill(map[x][y].getRed(), map[x][y].getGreen(), map[x][y].getBlue());
+        g.stroke(Math.max(map[x][y].getRed()-20, 0), Math.max(map[x][y].getGreen()-20, 0), Math.max(map[x][y].getBlue()-20, 0));
         g.rect(indexWidth * x, indexWidth * y, indexWidth, indexWidth);
       }
     }
-
+    
     if(makePiece){
       creator();
     }else{
       mover();
     }
-    drawGrid();
+    drawGrid(map);
   }
-  public void drawGrid(){
-    for (int x = 0; x < SubMap.length; x++) {
-      for (int y = 0; y < SubMap[x].length; y++) {
+  public void drawGrid(Color[][] map){
+    for (int x = 0; x < map.length; x++) {
+      for (int y = 0; y < map[x].length; y++) {
         stroke(0, 0, 0, 20);
         strokeWeight(1);
         line(0, y*indexWidth, worldWidth*indexWidth, y*indexWidth);
@@ -44,19 +45,19 @@ public class Sketch extends PApplet {
       }
     }
   }
-
-  public void clearLines(){
-    for (int y = 0; y < SubMap[0].length; y++) {
-      boolean clear = SubMap[0][y] != null;
-      for (int x = 1; x < SubMap.length && clear; x++) {
-        clear &= SubMap[x][y] != null;
+  
+  public void clearLines(Color[][] map){
+    for (int y = 0; y < map[0].length; y++) {
+      boolean clear = map[0][y] != null;
+      for (int x = 1; x < map.length && clear; x++) {
+        clear &= map[x][y] != null;
       }
       if(clear){
-        for (int i = 0; i < SubMap.length; i++) {
+        for (int i = 0; i < map.length; i++) {
           for (int j = y; j > 0; j--) {
-            SubMap[i][j] = SubMap[i][j-1];
+            map[i][j] = map[i][j-1];
           }
-          SubMap[i][0] = null;
+          map[i][0] = null;
         }
       }
     }
@@ -64,7 +65,7 @@ public class Sketch extends PApplet {
   
   @Override
   public void settings() {
-    size(worldWidth*indexWidth,worldHeight*indexWidth);
+    size(worldWidth*indexWidth * (makePiece ? 1 : 3),worldHeight*indexWidth * (makePiece ? 1 : 3));
   }
   
   public void keyPressed(){
@@ -82,10 +83,20 @@ public class Sketch extends PApplet {
       activePiece.rotateLeft(SubMap);
     }
     if(keyCode == DOWN){
-      aplyGravity();
+      try{
+        aplyGravity(makePiece ? SubMap : topMap);
+      }catch(RuntimeException e){
+        if(makePiece){
+          SubMap = null;
+          makePiece = false;
+          settings();
+        }else{
+          throw e;
+        }
+      }
     }
   }
-
+  
   private void creator() {
     if(activePiece == null){
       activePiece = pg.popPiece();
@@ -93,7 +104,7 @@ public class Sketch extends PApplet {
       return;
     }
     if(gravityTimer == 0){
-      aplyGravity();
+      aplyGravity(SubMap);
     }
     if(activePiece != null){
       activePiece.draw(g, indexWidth);
@@ -101,26 +112,32 @@ public class Sketch extends PApplet {
     incrimentGravity();
   }
   
-  private void aplyGravity() {
+  private void aplyGravity(Color[][] map) {
     if(activePiece == null){
       return;
     }
-    if(activePiece.y+activePiece.getHeight()+1 > worldHeight || activePiece.willCollide(SubMap)){
-      activePiece.Place(SubMap);
+    if(activePiece.y+activePiece.getHeight()+1 > worldHeight || activePiece.willCollide(map)){
+      activePiece.Place(map);
       activePiece.draw(g, indexWidth);
       activePiece = null;
+      makePiece = true;
       return;
     }
     activePiece.gravity();
   }
-
+  
   private void incrimentGravity() {
     gravityTimer++;
     gravityTimer %= 30;
   }
   
   private void mover() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'mover'");
+    if(gravityTimer == 0){
+      aplyGravity(topMap);
+    }
+    if(activePiece != null){
+      activePiece.draw(g, indexWidth);
+    }
+    incrimentGravity();
   }
 }
